60083592e568516c5ff9c39bb8b6751c21cebd01
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-11-11 22:16:58 +0500

    tugatdim

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+
+interface Todo {
+  id: number;
+  text: string;
+}
+
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    const savedTodos = localStorage.getItem("todos");
+    if (savedTodos) {
+      setTodos(JSON.parse(savedTodos));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() === "") return;
+    const newTodoItem = { id: Date.now(), text: newTodo };
+    setTodos([...todos, newTodoItem]);
+    setNewTodo("");
+  };
+
+  return (
+    <div className="container h-screen bg-center bg-no-repeat bg-cover bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
+      <div className="flex flex-col w-1/4 mx-auto gap-4 pt-5">
+        <h1 className="mb-4 text-3xl font-extrabold text-slate-50 dark:text-white md:text-5xl lg:text-6xl">
+          Todo App
+        </h1>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Add Todo"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+        <button
+          onClick={handleAddTodo}
+          className="text-white w-full bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 hover:transition-all duration-300"
+        >
+          ADD
+        </button>
+        <h4 className="text-white text-2xl mx-auto">TODOS</h4>
+        <ul className="mt-4">
+          {todos.map((todo) => (
+            <li
+              key={todo.id}
+              className="w-full max-w-sm p-2 bg-white border border-gray-200 rounded-lg shadow sm:p-4 dark:bg-gray-800 dark:border-gray-700 mb-3 hover:bg-slate-200 hover:scale-105"
+            >
+              {todo.text}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
